8496204f095c9df7160bb45baae217471a80b4da
Kasia Zalewska <kasia@codility.com>
2016-03-16 19:14:33 +0100

    fixes

--- create_table.cpp
+++ create_table.cpp
@@ -4,14 +4,16 @@
 
 using namespace std;
 
+vector<int> otoczka;
+vector<vector<vector<int> > > podzbiory_otoczki_blue;
+vector<vector<vector<int> > > podzbiory_otoczk_red;
+vector<vector<vector<int> > > choices;
+
 struct wynik {
 	int w;
 	int o;
 	int min;
 };
-
-otoczkaS currOtoczka;
-int otoczka = 4;
 
 void wypiszGraf(vector<vector<int> > &graf){
 	for(int i = 0;i<graf.size();i++){
@@ -25,7 +27,6 @@ void wypiszGraf(vector<vector<int> > &graf){
 void wczytajGraf(vector<vector<int> > &graf){
 	int size;
 	cin >> size;
-	blue = size;
 	for (int i = 0; i < size; i++) {
 		int m;
 		cin >> m;
@@ -52,10 +53,6 @@ set<int> kolorujOtoczke(vector<vector<int> > &graf, vector<int> wybrane, int oto
 	}
 	return res;
 }
-set<int> oto;
-vector<vector<vector<int> > > podzbiory;
-vector<vector<vector<int> > > podzbiory2;
-vector<vector<int> > podOt;
 
 void wypiszSet(set<int> &s){
 	for(set<int>::iterator iter=s.begin(); iter!=s.end();++iter) {
@@ -112,8 +109,6 @@ int countOtoczka(set<int> &s, int otoczka){
 
 		if (oto.find(*iter)!=oto.end()) res++;
 	}
-	//if (res==0) {wypiszSet(s);
-	//cout<<"\n\n";}
 	return res;
 }
 
@@ -135,11 +130,17 @@ inline bool operator<(const wynik& lhs, const wynik& rhs)
 	}
 	return lhs.w<rhs.w;
 }
-void wczytajOtoczke(){
-	podzbiory_otoczki =  {{{0}},{{1}},{{1},{2},{1,2}},{{1},{2},{3},{1,2},{1,3},{2,3},{1,2,3}},{{1},{2},{blue-1},{blue},{1,2},{1,blue-1},{1,blue},{2,blue-1},{2,blue},{blue-1,blue},{1,2,blue-1},{1,2,blue},{2,blue-1,blue},{1,2,blue-1,blue}}};
-	oto = {currOtoczka.w1,currOtoczka.w2,currOtoczka.w3,currOtoczka.w4};
-	podOt =  {{currOtoczka.w1},{currOtoczka.w2},{currOtoczka.w3},{currOtoczka.w4},{currOtoczka.w1,currOtoczka.w2},{currOtoczka.w1,currOtoczka.w3},{currOtoczka.w1,currOtoczka.w4},{currOtoczka.w2,currOtoczka.w3},{currOtoczka.w2,currOtoczka.w4},{currOtoczka.w3,currOtoczka.w4},{currOtoczka.w1,currOtoczka.w2,currOtoczka.w3},{currOtoczka.w1,currOtoczka.w2,currOtoczka.w4},{currOtoczka.w2,currOtoczka.w3,currOtoczka.w4},{currOtoczka.w1,currOtoczka.w2,currOtoczka.w3,currOtoczka.w4}};
-	podzbiory2 = {
+
+struct otoczka{
+      int w1;
+      int w2;
+      int w3;
+      int w4;
+};
+
+void wczytaj_otoczke_blue(){
+	podzbiory_otoczki_blue =  {{{0}},{{1}},{{1},{2},{1,2}},{{1},{2},{3},{1,2},{1,3},{2,3},{1,2,3}},{{1},{2},{blue-1},{blue},{1,2},{1,blue-1},{1,blue},{2,blue-1},{2,blue},{blue-1,blue},{1,2,blue-1},{1,2,blue},{2,blue-1,blue},{1,2,blue-1,blue}}};
+	choices = {
 		{{0}},
 		{{5}},
 		{{5},{6},{5,6}},
@@ -148,47 +149,48 @@ void wczytajOtoczke(){
 		{{3},{4},{5},{6},{7},{3,4},{3,5},{3,6},{3,7},{4,5},{4,6},{4,7},{5,6},{5,7},{6,7},{3,4,5},{3,4,6},{3,4,7},{3,5,6},{3,5,7},{3,6,7},{4,5,6},{4,5,7},{4,6,7},{5,6,7},{3,4,5,6},{3,4,5,7},{3,4,6,7},{3,5,6,7},{4,5,6,7},{3,4,5,6,7}},
 		{{3},{4},{5},{6},{7},{8},{3,4},{3,5},{3,6},{3,7},{3,8},{4,5},{4,6},{4,7},{4,8},{5,6},{5,7},{5,8},{6,7},{6,8},{3,4,5},{3,4,6},{3,4,7},{3,4,8},{3,5,6},{3,5,7},{3,5,8},{3,6,7},{3,6,8},{3,7,8},{4,5,6},{4,5,7},{4,5,8},{4,6,7},{4,6,8},{4,7,8},{5,6,7},{5,6,8},{5,7,8},{3,4,5,6},{3,4,5,7},{3,4,5,8},{3,4,6,7},{3,4,6,8},{3,4,7,8},{3,5,6,7},{3,5,6,8},{3,5,7,8},{4,5,6,7},{4,5,6,8},{4,5,7,8},{4,6,7,8},{5,6,7,8},{3,4,5,6,7},{3,4,5,6,8},{4,5,6,7,8},{3,4,5,6,7,8}},};
 }
+
+void wczytaj_otoczke_red(){
+  struct otoczka current_otoczka;
+  cin >> current_otoczka.w1 >> current_otoczka.w2 >> current_otoczka.w3 >> current_otoczka.w4;
+  otoczka = {current_otoczka.w1, current_otoczka.w2, current_otoczka.w3, current_otoczka.w4};
+	podzbiory_otoczki_red =  {{current_otoczka.w1},{current_otoczka.w2},{current_otoczka.w3},{current_otoczka.w4},{current_otoczka.w1,current_otoczka.w2},{current_otoczka.w1,current_otoczka.w3},{current_otoczka.w1,current_otoczka.w4},{current_otoczka.w2,current_otoczka.w3},{current_otoczka.w2,current_otoczka.w4},{current_otoczka.w3,current_otoczka.w4},{current_otoczka.w1,current_otoczka.w2,current_otoczka.w3},{current_otoczka.w1,current_otoczka.w2,current_otoczka.w4},{current_otoczka.w2,current_otoczka.w3,current_otoczka.w4},{current_otoczka.w1,current_otoczka.w2,current_otoczka.w3,current_otoczka.w4}};
+}
 int main(){
 	int n, blue, red;
 	set<vector<wynik> > wynikSet;
 	cin >> n >> blue >> red;
 	cerr<<"all graphs "<<n<<"\n";
+  wczytaj_otoczke_blue();
   //ONLY FOR TESTS TODO Change that!
   n = 1;
 	for (int i = 0;i<n;i++){
-		vector<int> wybraneOtoczka;
+		vector<int> wybrane_do_otoczki;
 		vector<vector<int> > graf;
 		wczytajGraf(graf);
 		wypiszGraf(graf);
+    wczytaj_otoczke_red();
 		vector<wynik> v;
-		for (int h = 0; h<podzbiory[otoczka].size();h++){
-			//	for (int h = 0; h<1;h++){
-
-			for (int h2 = 0; h2<podOt.size();h2++){
-				cerr<<"poczatek\n";
-				cerr<<"h "<<h<<" h2 "<<h2<<"blue"<<blue<<"\n";
-				set<int> s = kolorujOtoczke(graf,podzbiory[otoczka][h],otoczka);
+		for (int h = 0; h < podzbiory_otoczki_blue[otoczka].size(); h++){
+			for (int h2 = 0; h2 < choices.size(); h2++){
+				cerr<<"beginning of the loop\n";
+				set<int> s = kolorujOtoczke(graf, podzbiory_otoczki_blue[otoczka][h], otoczka);
 				addPodOt(s,h2);
 				//	wypiszSet(s);
 				cerr<<"koloruj\n";
-				int wybrane = podzbiory[otoczka][h].size();
-				cerr<<"wybrane "<<wybrane<<"\n";
+				int wybrane = podzbiory_otoczki_blue[otoczka][h].size();
+				cerr<<"ilosc wybranyc wierzcholkow "<<wybrane<<"\n";
 				int ot = countOtoczka(s, otoczka);
-				cerr<<"ot "<<ot<<"\n";
+				cerr<<"ilosc wierzcholkow na otoczce "<<ot<<"\n";
 
 				int naj = najmniejszeKolorowanie(graf,s,red,otoczka);
-				//			cout<<"naj "<<naj<<"\n";
+				cerr<<"najmniejsze kolorowanie dla tego wyboru "<<naj<<"\n";
 				wynik r;
 				r.w = wybrane;
 				r.o=ot;
 				r.min = naj;
-
-
-
 				//				if (szukaj(graf,r,2,4,3, podzbiory[otoczka][h])) return 1;
-				//cout<<"prawiekoniec\n";
 				v.push_back(r);
-				//cout<<"koniec\n";
 			}
 		}
 
