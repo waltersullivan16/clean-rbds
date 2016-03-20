1fb3ceb00f209dbe513a61596424e5f9a5821905
mello <walter.sullivan.16@gmail.com>
2016-03-20 22:38:34 +0100

    create_table is compiling

--- create_table.cpp
+++ create_table.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-vector<int> otoczka;
-vector<vector<vector<int> > > podzbiory_otoczki_blue;
-vector<vector<vector<int> > > podzbiory_otoczk_red;
-vector<vector<vector<int> > > choices;
+set<int> otoczka;
+vector<vector<int> > podzbiory_otoczki_blue;
+vector<vector<int> > podzbiory_otoczki_red;
+vector<vector<int> > choices;
 
 struct wynik {
 	int w;
@@ -40,7 +40,7 @@ void wczytajGraf(vector<vector<int> > &graf){
 	}
 }
 
-set<int> kolorujOtoczke(vector<vector<int> > &graf, vector<int> wybrane, int otoczka){
+set<int> kolorujOtoczke(vector<vector<int> > &graf, vector<int> wybrane){
 	set<int> res;
 	cerr<<"size "<<wybrane.size()<<"\n";
 	for(int i = 0; i<wybrane.size();i++)
@@ -71,22 +71,21 @@ void wypiszVectorWynik(set<vector<wynik> > &s){
 	cout<<"\n";
 }
 
-int najmniejszeKolorowanie(vector<vector<int> > & graf, set<int> &pokolorowane, int red, int otoczka){
+int najmniejszeKolorowanie(vector<vector<int> > & graf, set<int> &pokolorowane, int red){
 	if (pokolorowane.size() == red) return 0;
 	int res = -1;
-	int ile = graf.size()-otoczka;
-	for(int i = 0; i<podzbiory2[ile].size();i++){
+	for(int i = 0; i<choices.size();i++){
 		set<int> pok = pokolorowane;
 		//		cout<<"red "<<red<<" ile "<<ile<<" otoczka "<<otoczka<<" pokolorowane "<<pokolorowane.size()<<" "<<graf.size()<<" "<<otoczka<<"\n";
-		for(int j = 0; j<podzbiory2[ile][i].size();j++){
+		for(int j = 0; j<choices[i].size();j++){
 			//	cout<<"ile "<<ile<<" i "<<i<<" j "<<j<<" pod "<<podzbiory2[ile][i][j]-1<<"\n";
-			for (int z = 0; z <graf[podzbiory2[ile][i][j]-1].size();z++) {
-				pok.insert(graf[podzbiory2[ile][i][j]-1][z]);
+			for (int z = 0; z <graf[choices[i][j]-1].size();z++) {
+				pok.insert(graf[choices[i][j]-1][z]);
 				//cout<<graf[podzbiory2[ile][i][j]-1][z]<<" ";
 			}
 		}
 		//	cout<<"koniec\n";
-		if (pok.size()==red) return podzbiory2[ile][i].size();
+		if (pok.size()==red) return choices[i].size();
 	}
 	//cout<<"koniec\n";
 	return res;
@@ -103,23 +102,22 @@ bool szukaj(vector<vector<int> >& graf, wynik r, int w, int o, int min, vector<i
 	return false;
 }
 
-int countOtoczka(set<int> &s, int otoczka){
+int countOtoczka(set<int> &s){
 	int res = 0;
 	for(set<int>::iterator iter=s.begin(); iter!=s.end();++iter) {
 
-		if (oto.find(*iter)!=oto.end()) res++;
+		if (otoczka.find(*iter)!=otoczka.end()) res++;
 	}
 	return res;
 }
 
 void addPodOt(set<int> &s, int h){
-	for(int i =0; i<podOt[h].size();i++) {
+	for(int i =0; i<podzbiory_otoczki_red[h].size();i++) {
 
-		s.insert(podOt[h][i]);
+		s.insert(podzbiory_otoczki_red[h][i]);
 	}
-	//if (res==0) {wypiszSet(s);
-	//cout<<"\n\n";}
 }
+
 inline bool operator<(const wynik& lhs, const wynik& rhs)
 {
 	if (lhs.w==rhs.w) {
@@ -138,16 +136,10 @@ struct otoczka{
       int w4;
 };
 
-void wczytaj_otoczke_blue(){
-	podzbiory_otoczki_blue =  {{{0}},{{1}},{{1},{2},{1,2}},{{1},{2},{3},{1,2},{1,3},{2,3},{1,2,3}},{{1},{2},{blue-1},{blue},{1,2},{1,blue-1},{1,blue},{2,blue-1},{2,blue},{blue-1,blue},{1,2,blue-1},{1,2,blue},{2,blue-1,blue},{1,2,blue-1,blue}}};
+void wczytaj_otoczke_blue(int blue){
+	podzbiory_otoczki_blue =  {{1},{2},{blue-1},{blue},{1,2},{1,blue-1},{1,blue},{2,blue-1},{2,blue},{blue-1,blue},{1,2,blue-1},{1,2,blue},{2,blue-1,blue},{1,2,blue-1,blue}};
 	choices = {
-		{{0}},
-		{{5}},
-		{{5},{6},{5,6}},
-		{{5},{6},{7},{5,6},{5,7},{6,7},{5,6,7}},
-		{{3},{4},{5},{6},{3,4},{3,5},{3,6},{4,5},{4,6},{5,6},{3,4,5},{3,4,6},{3,5,6},{4,5,6},{3,4,5,6}},
-		{{3},{4},{5},{6},{7},{3,4},{3,5},{3,6},{3,7},{4,5},{4,6},{4,7},{5,6},{5,7},{6,7},{3,4,5},{3,4,6},{3,4,7},{3,5,6},{3,5,7},{3,6,7},{4,5,6},{4,5,7},{4,6,7},{5,6,7},{3,4,5,6},{3,4,5,7},{3,4,6,7},{3,5,6,7},{4,5,6,7},{3,4,5,6,7}},
-		{{3},{4},{5},{6},{7},{8},{3,4},{3,5},{3,6},{3,7},{3,8},{4,5},{4,6},{4,7},{4,8},{5,6},{5,7},{5,8},{6,7},{6,8},{3,4,5},{3,4,6},{3,4,7},{3,4,8},{3,5,6},{3,5,7},{3,5,8},{3,6,7},{3,6,8},{3,7,8},{4,5,6},{4,5,7},{4,5,8},{4,6,7},{4,6,8},{4,7,8},{5,6,7},{5,6,8},{5,7,8},{3,4,5,6},{3,4,5,7},{3,4,5,8},{3,4,6,7},{3,4,6,8},{3,4,7,8},{3,5,6,7},{3,5,6,8},{3,5,7,8},{4,5,6,7},{4,5,6,8},{4,5,7,8},{4,6,7,8},{5,6,7,8},{3,4,5,6,7},{3,4,5,6,8},{4,5,6,7,8},{3,4,5,6,7,8}},};
+		{3},{4},{5},{6},{7},{8},{3,4},{3,5},{3,6},{3,7},{3,8},{4,5},{4,6},{4,7},{4,8},{5,6},{5,7},{5,8},{6,7},{6,8},{3,4,5},{3,4,6},{3,4,7},{3,4,8},{3,5,6},{3,5,7},{3,5,8},{3,6,7},{3,6,8},{3,7,8},{4,5,6},{4,5,7},{4,5,8},{4,6,7},{4,6,8},{4,7,8},{5,6,7},{5,6,8},{5,7,8},{3,4,5,6},{3,4,5,7},{3,4,5,8},{3,4,6,7},{3,4,6,8},{3,4,7,8},{3,5,6,7},{3,5,6,8},{3,5,7,8},{4,5,6,7},{4,5,6,8},{4,5,7,8},{4,6,7,8},{5,6,7,8},{3,4,5,6,7},{3,4,5,6,8},{4,5,6,7,8},{3,4,5,6,7,8}};
 }
 
 void wczytaj_otoczke_red(){
@@ -161,7 +153,7 @@ int main(){
 	set<vector<wynik> > wynikSet;
 	cin >> n >> blue >> red;
 	cerr<<"all graphs "<<n<<"\n";
-  wczytaj_otoczke_blue();
+  wczytaj_otoczke_blue(blue);
   //ONLY FOR TESTS TODO Change that!
   n = 1;
 	for (int i = 0;i<n;i++){
@@ -171,19 +163,19 @@ int main(){
 		wypiszGraf(graf);
     wczytaj_otoczke_red();
 		vector<wynik> v;
-		for (int h = 0; h < podzbiory_otoczki_blue[otoczka].size(); h++){
-			for (int h2 = 0; h2 < choices.size(); h2++){
+		for (int h = 0; h < choices.size(); h++){
+			for (int h2 = 0; h2 < podzbiory_otoczki_red.size(); h2++){
 				cerr<<"beginning of the loop\n";
-				set<int> s = kolorujOtoczke(graf, podzbiory_otoczki_blue[otoczka][h], otoczka);
+				set<int> s = kolorujOtoczke(graf,choices[h]);
 				addPodOt(s,h2);
 				//	wypiszSet(s);
 				cerr<<"koloruj\n";
-				int wybrane = podzbiory_otoczki_blue[otoczka][h].size();
+				int wybrane = podzbiory_otoczki_blue[h].size();
 				cerr<<"ilosc wybranyc wierzcholkow "<<wybrane<<"\n";
-				int ot = countOtoczka(s, otoczka);
+				int ot = countOtoczka(s);
 				cerr<<"ilosc wierzcholkow na otoczce "<<ot<<"\n";
 
-				int naj = najmniejszeKolorowanie(graf,s,red,otoczka);
+				int naj = najmniejszeKolorowanie(graf,s,red);
 				cerr<<"najmniejsze kolorowanie dla tego wyboru "<<naj<<"\n";
 				wynik r;
 				r.w = wybrane;
